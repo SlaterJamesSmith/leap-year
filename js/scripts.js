1d9012ddd48ec268c514a9c8cd5f5d8e6dc76181
Slater Smith <smithslater@gmail.com>
2019-01-24 13:56:17 -0800

    get initial true or false function working

---
+++ js/scripts.js
@@ -0,0 +1,23 @@
+$(document).ready(function() {
+  $("form#leap-year").submit(function(event) {
+    event.preventDefault();
+
+
+
+    var leapYear = function(year) {
+      if ((year % 4) === 0) {
+        return true;
+      } else {
+        return false;
+      }
+    };
+
+    var year = parseInt($("input#year").val());
+    var result = leapYear(year);
+
+    console.log(leapYear);
+
+
+    $("#result").text(result);
+  });
+});
